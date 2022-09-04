71384d68486719a55503a9e8cd164e3ab06ba9e6
Ryo FUKUNAGA <truefreedom.heart.and.soul@gmail.com>
2022-09-04 15:15:14 +0900

    写経中20220904-1

--- code/flaskr/__init__.py
+++ code/flaskr/__init__.py
@@ -1,8 +1,14 @@
+import os
+#osモジュールを使えるようにする
+
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+#flaskパッケージから、Flaskクラスをインポート
 
-app = Flask(__name__)
-app.config.from_object('flaskr.config')
-db = SQLAlchemy(app)
-
-import flaskr.views
+def create_app(test_config=None):
+#create_app()という変数の定義開始 引数test_configのデフォルト値がNone
+  app = Flask(__name__, instance_relative_config=True)
+  #appにFlaskインスタンスを代入
+  app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )

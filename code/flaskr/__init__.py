e0006ca0fc8c255dcf6a90fd3814ea901f3e6c73
Ryo FUKUNAGA <truefreedom.heart.and.soul@gmail.com>
2022-09-06 21:18:57 +0900

    20220906 インスタンスフォルダの存在チェックまで

--- code/flaskr/__init__.py
+++ code/flaskr/__init__.py
@@ -12,3 +12,18 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'), #インスタンスフォルダ直下に、SQLiteデータベースを配置
     )
+  
+  if test_config is none:
+    # instance configを読み込む テスト環境じゃないとき
+    app.config.from_pyfile('config.py', silent=True)
+  else:
+    # テストコンフィグを読み込む
+    app.config.from_mapping(test_config)
+  
+  #インスタンスフォルダの存在チェック
+  try:
+    os.makedirs(app.instance_path) #インスタンスパスのフォルダを作成
+  except OSError:
+    pass #すでにインスタンスパスのフォルダが存在するときはエラーを吐く。その場合は何も実行しない
+
+    

f1a4620b1baa0f05085055747c8f0573b4c3d3ca
Ryo FUKUNAGA <truefreedom.heart.and.soul@gmail.com>
2022-09-13 20:46:35 +0900

    20220913_表作成の手前まで

--- code/flaskr/db.py
+++ code/flaskr/db.py
@@ -13,3 +13,12 @@ def get_db():
       detect_types=sqlite3.PARSE_DECLTYPES
     )
     g.db.row_factory = sqlite3.Row
+  
+  return g.db
+
+#g.dbが設定されているかを調べ、設定されている場合はconnectionを閉じる関数。各リクエストの後にclose_db()を呼び出すことになるようだ。
+def close_db(e=None):
+  db = g.pop('db', None) #gからdb要素を持ってくる。存在しない場合はNoneを返す。
+
+  if db is not None: #dbが存在している場合は
+    db.close()       #connectionを閉じる

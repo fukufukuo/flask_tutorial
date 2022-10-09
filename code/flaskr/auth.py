8a32df82c15fff1cf4c7bbaef1065db6f4e0f4bb
Ryo FUKUNAGA <truefreedom.heart.and.soul@gmail.com>
2022-10-09 16:53:57 +0900

    20221009_modified auth.py

--- code/flaskr/auth.py
+++ code/flaskr/auth.py
@@ -1,6 +1,7 @@
 #ここで作成するblueprintは、新しいユーザの登録と、ログイン/ログアウトのviewを持つようにする
 
 #いんぽーと
+from crypt import methods
 import functools
 from flask import (
   Blueprint, flash, g, redirect, render_template, request, session, url_for
@@ -12,3 +13,5 @@ from flaskr.db import get_db #flaskrディレクトリ内のdb.pyから関数を
 bp = Blueprint('auth', __name__, url_prefix='./auth') #__name__には、このファイルをモジュールとして読み込む場合はモジュール名、スクリプトとして実行する場合は__main__が入るのだった。
 #url_prefixは、blueprintのURLの最初につけるパスのこと。
 
+#ルーティング開始
+@bp.route('/register', methods=('GET', 'POST')) #GET, POSTメソッドの使用を許可。
